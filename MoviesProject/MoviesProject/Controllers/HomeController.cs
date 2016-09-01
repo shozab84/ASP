e663d9f57eff3fabee24f165d7cf52db327d5628
Syed Shozab Hasan Rizvi <shozab.rizvi84@gmail.com>
2016-09-01 16:32:51 +0100

    1st Sept class
    1st Sept class

---
+++ MoviesProject/MoviesProject/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using MoviesProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoviesProject.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private MoviesDBShozabEntities db = new MoviesDBShozabEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            //LINQ query to get records
+            var movies = from m in db.Movies
+                         select m;
+            return View(movies);
+        }
+    }
+}
